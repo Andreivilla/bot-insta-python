edaf80eb4c1699c51ed68189ea596ea115fd4095
AndreiVIlla <andreivilla63@gmaIL.com>
2020-04-28 00:16:37 -0300

    não encontrei o xpath
    não encontrei o xpath das imagens no instagram

--- InstagramBot.py
+++ InstagramBot.py
@@ -3,15 +3,32 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 class InstagramBot:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self):
         self.driver = webdriver.Firefox(executable_path = r'C:\Users\Cliente\Documents\GitHub\bot-insta-python\geckodriver.exe')
 
-    def login(self):
+    def login(self, username, password):# a função lloguiun deve receber loguin e senha
         driver = self.driver
         driver.get('https://www.instagram.com/?hl=pt-br')
+        time.sleep(5)
+        
+        #entrada do nome do perfil
+        user = driver.find_element_by_xpath("//input[@name='username']")# Passa o caminho do usuario
+        user.clear()
+        user.send_keys(username)#preenche o campo do usuario com o nome passado pelo usuario
+        
+        #entrada da senha do perfil
+        password_item = driver.find_element_by_xpath("//input[@type='password']")# Passa o caminho da senha
+        password_item.clear()
+        password_item.send_keys(password)#preenche o campo do usuario com a senha passada pelo usuario
+        password_item.send_keys(Keys.RETURN)
+        time.sleep(5)
 
+    def follow(self, page):
+        driver = self.driver
+        
+        driver.get(page)
 
-Bot = InstagramBot('adwadwa','wdawdwf')
-Bot.login()
+        driver.find_element_by_xpath('//a[@href="/python.bot/followers/"]').click()
+
+        #//a[@href="/python.bot/followers/"]
+        #<a class="-nal3 " href="/python.bot/followers/"><span class="g47SY " title="64">64</span> seguidores</a>
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from InstagramBot import InstagramBot
 
-Bot = InstagramBot('adwadwa','wdawdwf')
-Bot.login()
+#usuario = str(input('Usuario: '))
+#senha = str(input('Senha: '))
+
+usuario = 'andreivilla'
+senha = '36461023'
+
+Bot = InstagramBot()
+Bot.login(usuario, senha)
+Bot.follow('https://www.instagram.com/caiobotturapro/?hl=pt-br')
+
+
+#https://www.instagram.com/python.bot/?hl=pt-br
+#https://www.instagram.com/p/B6qz-PXpslF/
+
+#
+#
